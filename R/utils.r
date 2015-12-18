05a88dcbb98406df0fa94c4912bab803ee0b2587
robinlovelace <rob00x@gmail.com>
2015-12-18 01:53:33 +0000

    Change bbox order (bug fix)

--- R/utils.r
+++ R/utils.r
@@ -65,9 +65,9 @@ bbox_to_string <- function(bbox) {
     } else {
       if (length(bbox) > 1 & length(bbox) == 4) {
         if (all(names(bbox) %in% c("left", "bottom", "right", "top"))) {
-          bbox <- paste0(bbox[c("left", "bottom", "right", "top")], collapse=",")
+          bbox <- paste0(bbox[c("bottom", "left", "top", "right")], collapse=",")
         } else {
-          bbox <- paste0(bbox, collapse=",")
+          bbox <- paste0(bbox[c(2,1,4,3)], collapse=",")
         }
       }
     }
